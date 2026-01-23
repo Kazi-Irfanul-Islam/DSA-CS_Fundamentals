aa90c54cb6532653ad6c111957f6dbaf9358bfb9
Kazi-Irfanul-Islam <irfanulislam01851@gmail.com>
2026-01-23 18:37:05 +0600

    Insertion in LL

--- LinkedList/Learning/Deletion_in_a_LL.cpp
+++ LinkedList/Learning/Deletion_in_a_LL.cpp
@@ -1,31 +1,117 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node {
-    public:
+public:
     int data;
     Node* next;
-    Node(int x) {
+
+    Node(int x, Node* nxt = nullptr) {
         data = x;
-        next = nullptr;
+        next = nxt;
     }
 };
 
+/* ------------------ INSERT ------------------ */
+
+// Insert at head
+Node* insertHead(Node* head, int val) {
+    return new Node(val, head);
+}
+
+// Insert at tail
+Node* insertTail(Node* head, int val) {
+    Node* newNode = new Node(val);
+
+    if(head == nullptr) return newNode;
+
+    Node* temp = head;
+    while(temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return head;
+}
+
+// Insert at K-th position (1-indexed)
+Node* insertPosition(Node* head, int el, int k) {
+    if(head == NULL) {
+        if(k == 1) return new Node(el);
+        else return head;
+    }
+
+    if(k == 1) return new Node(el, head);
+
+    int cnt = 0;
+    Node* temp = head;
+    while(temp != NULL) {
+        cnt++;
+        if(cnt == k - 1) {
+            Node* newNode = new Node(el, temp->next);
+            temp->next = newNode;
+            return head;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+// Insert Before specific value
+Node* insertBeforeValue(Node* head, int el, int val) {
+    if(head == NULL) return NULL;
+    if(head->data == val) return new Node(el, head);
+
+    Node* temp = head;
+    while(temp->next != NULL) {
+        if(temp->next->data == val) {
+            Node* newNode = new Node(el, temp->next);
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+// Insert After specific value
+Node* insertAfterValue(Node* head, int el, int val) {
+    if(head == NULL) return NULL;
+    if(head->data == val) return new Node(el, head->next);
+
+    Node* temp = head;
+    while(temp->next != NULL) {
+        if(temp->data == val) {
+            Node* newNode = new Node(el, temp->next);
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+/* ------------------ DELETE ------------------ */
+
+// Remove head
 Node* removeHead(Node* head) {
-    if(head == nullptr) {
+    if(head == nullptr) return nullptr;
+
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+// Remove tail
+Node* removeTail(Node* head) {
+    if(head == nullptr) return nullptr;
+
+    if(head->next == nullptr) {
+        delete head;
         return nullptr;
     }
 
-    Node* temp = head;
-    head = head -> next;
-    delete temp;
-
-    return head;
-} 
-
-Node* removeTail(Node* head) {
-    if(head == nullptr or head->next == nullptr) return nullptr;
-    
     Node* temp = head;
     while(temp->next->next != nullptr) {
         temp = temp->next;
@@ -33,81 +119,99 @@ Node* removeTail(Node* head) {
 
     delete temp->next;
     temp->next = nullptr;
-
     return head;
 }
 
-Node* removeKthElement(Node* head, int pos) {
-    if(head == NULL) return NULL;
-    
-    if(pos == 1) {
-        Node* temp = head;
-        head = head->next;
-        free(temp);
-        return head;
+// Remove K-th node (1-indexed)
+Node* removeKth(Node* head, int k) {
+    if(head == nullptr || k <= 0) return head;
+
+    if(k == 1) {
+        return removeHead(head);
     }
 
-    int cnt = 0;
-    Node* temp = head;
-    Node* prev = NULL;
+    Node* prev = head;
+    Node* curr = head->next;
+    int pos = 2;
 
-    while(temp != NULL) {
-        cnt++;
-        if(cnt == pos) {
-            prev->next = prev->next->next;
-            free(temp);
+    while(curr != nullptr) {
+        if(pos == k) {
+            prev->next = curr->next;
+            delete curr;
             break;
         }
-        prev = temp;
-        temp = temp -> next;
+        prev = curr;
+        curr = curr->next;
+        pos++;
     }
 
     return head;
 }
 
-Node* removeElement(Node* head, int val) {
-    if(head == NULL) return NULL;
-    if(head -> data == val) {
-        Node* temp = head;
-        head = head -> next;
-        free(temp);
-        return head;
+// Remove first occurrence of value
+Node* removeValue(Node* head, int val) {
+    if(head == nullptr) return nullptr;
+
+    if(head->data == val) {
+        return removeHead(head);
     }
 
-    Node* temp = head;
-    Node* prev = NULL;
-    while(temp != NULL) {
-        if(temp -> data == val) {
-            prev -> next = prev -> next -> next;
-            free(temp);
+    Node* prev = head;
+    Node* curr = head->next;
+
+    while(curr != nullptr) {
+        if(curr->data == val) {
+            prev->next = curr->next;
+            delete curr;
             break;
         }
-        prev = temp;
-        temp = temp -> next;
-
+        prev = curr;
+        curr = curr->next;
     }
 
     return head;
 }
+
+/* ------------------ UTILITY ------------------ */
 
 void print(Node* head) {
-    while(head != NULL) {
-        cout << head -> data << endl;
-        head = head -> next;
+    while(head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
     }
-    cout << endl;
-} 
+    cout << "\n";
+}
+
+// Free entire list (important in CP & real apps)
+void freeList(Node* head) {
+    while(head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
 
 int main() {
     Node* head = new Node(1);
-    head->next = new Node(2);
-    head->next->next = new Node(3);
-    head->next->next->next = new Node(4);
+    head->next = new Node(2); 
+    head->next->next = new Node(3); 
+    head->next->next->next = new Node(4); 
     head->next->next->next->next = new Node(5);
     
+    // Insertion 
+    // head = insertHead(head, 100); 
+    // head = insertTail(head, 100); 
+    // head = insertPosition(head, 100, 3); 
+    // head = insertBeforeValue(head, 100, 3); 
+    head = insertAfterValue(head, 100, 3);
+
+    // Deletion 
     // head = removeHead(head);
-    // head = removeTail(head);
-    // head = removeKthElement(head, 3);
-    // head = removeElement(head, 1);
+    // head = removeTail(head); 
+    // head = removeKthElement(head, 3); 
+    // head = removeElement(head, 1); 
     print(head);
+
+    freeList(head);
 }
